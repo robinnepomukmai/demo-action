c977af35d4abe193ab06118900d182e23596d745
Robin Nepomuk Mai <robin@nepomuk.software>
2019-12-31 14:49:52 +0100

    stuff

--- dist/index.js
+++ dist/index.js
@@ -78,6 +78,8 @@ function runAction() {
         try {
             const input = core.getInput('who-to-greet');
             core.setOutput('greeting', `hello ${input}`);
+            // eslint-disable-next-line no-console
+            console.log(`hello ${input}`);
         }
         catch (error) {
             core.setFailed(error.message);
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ async function runAction(): Promise<void> {
   try {
     const input = core.getInput('who-to-greet')
     core.setOutput('greeting', `hello ${input}`)
+    // eslint-disable-next-line no-console
+    console.log(`hello ${input}`)
   } catch (error) {
     core.setFailed(error.message)
   }
